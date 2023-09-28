f4f9656891e73f9511623509a15046dfcd51a96f
Stella Hada <91349698+stellahada@users.noreply.github.com>
2023-09-28 18:02:29 -0300

    Add files via upload

---
+++ PetFinder 4/PetFinder/Notificacoes.swift
@@ -0,0 +1,23 @@
+
+import SwiftUI
+
+struct Notificacoes: View {
+    var animal: Pets
+   @State var animais : [Pets] = []
+    var body: some View {
+        Text(animal.nome!)
+            .font(.title)
+            .fontWeight(.bold)
+            .foregroundColor(Color("Color2"))
+        Image(animal.image)
+            .resizable().frame(width: 150,height: 150)
+        Spacer()
+        Text(animal.desc!)
+        Spacer()
+        NavigationLink(destination: ChatView()){
+            Text("Entrar em contato")
+        }
+    }
+}
+
+
